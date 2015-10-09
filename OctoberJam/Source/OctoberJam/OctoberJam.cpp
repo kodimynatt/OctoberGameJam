04a86b6b359289a1be894d5c57d3d73425ea2935
jackwpickrell@gmail.com <prawnzz@gmail.com>
2015-10-08 19:44:52 -0700

    Added c++ character class and undated .gitignore

---
+++ OctoberJam/Source/OctoberJam/OctoberJam.cpp
@@ -0,0 +1,5 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "OctoberJam.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, OctoberJam, "OctoberJam" );
